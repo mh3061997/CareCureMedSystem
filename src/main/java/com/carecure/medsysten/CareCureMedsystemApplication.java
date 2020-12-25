a4f122305342a254d979ddda4ab738179a0231f1
Mohamed Haitham <mh3061997@gmail.com>
2020-12-25 16:11:56 +0200

    Added Maven Dependencies and Packages Hierarchy

---
+++ src/main/java/com/carecure/medsysten/CareCureMedsystemApplication.java
@@ -0,0 +1,13 @@
+package com.carecure.medsysten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareCureMedsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareCureMedsystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carecure/medsysten/CareCureMedsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carecure.medsysten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareCureMedsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
