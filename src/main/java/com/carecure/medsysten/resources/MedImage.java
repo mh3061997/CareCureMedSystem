977c379471691fe0930ec77cc29416a280e5e880
Mohamed Haitham <mh3061997@gmail.com>
2020-12-25 17:46:31 +0200

    All resources finished without inheritance and relations

--- src/main/java/com/carecure/medsysten/resources/MedImage.java
+++ src/main/java/com/carecure/medsysten/resources/MedImage.java
@@ -1,4 +1,79 @@
 package com.carecure.medsysten.resources;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
 public class MedImage {
+    @Id
+    private long code;
+    private String image;
+    private String type; // prescription , radiology, lab
+
+    @Basic
+    @Temporal(TemporalType.DATE)
+    private Date dateAdded;
+
+    private long patientCode;
+    private String patientName;
+
+    public long getCode() {
+        return code;
+    }
+
+    public void setCode(long code) {
+        this.code = code;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Date getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(Date dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+    public long getPatientCode() {
+        return patientCode;
+    }
+
+    public void setPatientCode(long patientCode) {
+        this.patientCode = patientCode;
+    }
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public void setPatientName(String patientName) {
+        this.patientName = patientName;
+    }
+
+    public MedImage(long code, String image, String type, Date dateAdded, long patientCode, String patientName) {
+        this.code = code;
+        this.image = image;
+        this.type = type;
+        this.dateAdded = dateAdded;
+        this.patientCode = patientCode;
+        this.patientName = patientName;
+    }
+
+    public MedImage() {
+    }
 }
--- src/main/java/com/carecure/medsysten/resources/invoice.java
+++ src/main/java/com/carecure/medsysten/resources/invoice.java
@@ -1,4 +1,136 @@
 package com.carecure.medsysten.resources;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
 public class invoice {
+
+    @Id
+    private long code;
+
+    @Basic
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dateCreated;
+
+    @Basic
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dateFinalized;
+
+    private long totalDue;
+    private long totalPaid;
+    private long totalRemaining;
+
+    private String status;
+
+    private long patientCode;
+    private long patientName;
+    private long appointementCode;
+
+    private String paymentMethod;
+
+    public invoice() {
+    }
+
+    public invoice(long code, Date dateCreated, Date dateFinalized, long totalDue, long totalPaid, long totalRemaining, String status, long patientCode, long patientName, long appointementCode, String paymentMethod) {
+        this.code = code;
+        this.dateCreated = dateCreated;
+        this.dateFinalized = dateFinalized;
+        this.totalDue = totalDue;
+        this.totalPaid = totalPaid;
+        this.totalRemaining = totalRemaining;
+        this.status = status;
+        this.patientCode = patientCode;
+        this.patientName = patientName;
+        this.appointementCode = appointementCode;
+        this.paymentMethod = paymentMethod;
+    }
+
+    public long getCode() {
+        return code;
+    }
+
+    public void setCode(long code) {
+        this.code = code;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Date getDateFinalized() {
+        return dateFinalized;
+    }
+
+    public void setDateFinalized(Date dateFinalized) {
+        this.dateFinalized = dateFinalized;
+    }
+
+    public long getTotalDue() {
+        return totalDue;
+    }
+
+    public void setTotalDue(long totalDue) {
+        this.totalDue = totalDue;
+    }
+
+    public long getTotalPaid() {
+        return totalPaid;
+    }
+
+    public void setTotalPaid(long totalPaid) {
+        this.totalPaid = totalPaid;
+    }
+
+    public long getTotalRemaining() {
+        return totalRemaining;
+    }
+
+    public void setTotalRemaining(long totalRemaining) {
+        this.totalRemaining = totalRemaining;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public long getPatientCode() {
+        return patientCode;
+    }
+
+    public void setPatientCode(long patientCode) {
+        this.patientCode = patientCode;
+    }
+
+    public long getPatientName() {
+        return patientName;
+    }
+
+    public void setPatientName(long patientName) {
+        this.patientName = patientName;
+    }
+
+    public long getAppointementCode() {
+        return appointementCode;
+    }
+
+    public void setAppointementCode(long appointementCode) {
+        this.appointementCode = appointementCode;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
 }
