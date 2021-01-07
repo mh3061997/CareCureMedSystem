75fda394918a6b84a5103710047881b07b599d8b
Mohamed Haitham <mh3061997@gmail.com>
2021-01-07 18:04:52 +0200

    added datemade field to medimage

--- src/main/java/com/carecure/medsysten/resources/resMedImage.java
+++ src/main/java/com/carecure/medsysten/resources/resMedImage.java
@@ -16,12 +16,25 @@ public class resMedImage {
     private long code;
     private String image;
     private String type; // prescription , radiology, lab
-
+    private String subType; //sonar , makta3y , etc.
+    private String organ;   //sadr , kela .etc.
     @Basic
     @Temporal(TemporalType.DATE)
     private Date dateAdded;
 
-   @ManyToOne(fetch = FetchType.LAZY)
+    @Basic
+    @Temporal(TemporalType.DATE)
+    private Date dateMade;
+
+    public Date getDateMade() {
+        return dateMade;
+    }
+
+    public void setDateMade(Date dateMade) {
+        this.dateMade = dateMade;
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
    @JoinColumn(name = "patientCode")
    @JsonBackReference //never will i access medimages directly to keep controller clean
    private resPatient patient;
@@ -67,11 +80,30 @@ public class resMedImage {
         this.patient = patient;
     }
 
-    public resMedImage(long code, String image, String type, Date dateAdded, resPatient patient) {
+    public String getSubType() {
+        return subType;
+    }
+
+    public void setSubType(String subType) {
+        this.subType = subType;
+    }
+
+    public String getOrgan() {
+        return organ;
+    }
+
+    public void setOrgan(String organ) {
+        this.organ = organ;
+    }
+
+    public resMedImage(long code, String image, String type, String subType, String organ, Date dateAdded, Date dateMade, resPatient patient) {
         this.code = code;
         this.image = image;
         this.type = type;
+        this.subType = subType;
+        this.organ = organ;
         this.dateAdded = dateAdded;
+        this.dateMade = dateMade;
         this.patient = patient;
     }
 
