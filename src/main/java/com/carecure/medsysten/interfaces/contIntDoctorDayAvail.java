0046f6fe24fdb5fbfb5331c17ff900dff8685acf
mhaitham <mhaitham@sumerge.com>
2021-06-02 01:41:24 +0200

    Fixed bug where removing all doctor days avail in updatemulti throws nullpointer exception

--- src/main/java/com/carecure/medsysten/interfaces/contIntDoctorDayAvail.java
+++ src/main/java/com/carecure/medsysten/interfaces/contIntDoctorDayAvail.java
@@ -1,12 +1,12 @@
 package com.carecure.medsysten.interfaces;
 
 import com.carecure.medsysten.resources.resDoctorDayAvail;
-import com.carecure.medsysten.resources.resInvoiceItem;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RequestMapping("/doctorDayAvail")
@@ -28,7 +28,7 @@ public interface contIntDoctorDayAvail {
     void updateDoctorDayAvail(@PathVariable("doctorDayAvailCode") long code, @RequestBody resDoctorDayAvail updatedDoctorDayAvail);
 
     @RequestMapping(method = RequestMethod.PUT,value="/updatemulti")
-    void   updateDoctorDayAvailMulti(@RequestBody List<resDoctorDayAvail> updatedDoctorDayAvailArr);
+    void   updateDoctorDayAvailMulti(@PathParam("doctorCode") long doctorCode,@RequestBody List<resDoctorDayAvail> updatedDoctorDayAvailArr);
 
 
     @RequestMapping(method= RequestMethod.DELETE,value="/{doctorDayAvailCode}")
