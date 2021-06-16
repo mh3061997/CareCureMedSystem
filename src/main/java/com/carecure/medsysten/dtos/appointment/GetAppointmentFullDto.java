601cce9ac6518e53ab12ad1815520e37ac8ac18d
mhaitham <mhaitham@sumerge.com>
2021-06-17 00:18:30 +0200

    Modified Appointment Dtos to work with Enums

--- src/main/java/com/carecure/medsysten/dtos/appointment/GetAppointmentFullDto.java
+++ src/main/java/com/carecure/medsysten/dtos/appointment/GetAppointmentFullDto.java
@@ -1,24 +1,30 @@
 package com.carecure.medsysten.dtos.appointment;
 
+import com.carecure.medsysten.dtos.doctor.GetDoctorSimpleDto;
+import com.carecure.medsysten.dtos.patient.GetAllPatientDto;
 import com.carecure.medsysten.enums.appointment.EnumAppointmentStatus;
 import com.carecure.medsysten.enums.appointment.EnumAppointmentType;
 import com.carecure.medsysten.enums.misc.EnumSpeciality;
+import com.carecure.medsysten.resources.resNoteAppointment;
 import lombok.Data;
 
 import java.util.Date;
+import java.util.List;
 
 @Data
-public class NewAppointmentDto
+public class GetAppointmentFullDto
 {
-
+	private long code;
 	private EnumSpeciality speciality;
 	private Date dateCreated;
 	private Date dateToVisit;
+	private EnumAppointmentStatus status;
 	private EnumAppointmentType type;
 	private String creationNote;
 	private String userCreatedBy;
-	private long patientCode;
-	private long doctorCode;
-	private EnumAppointmentStatus status = EnumAppointmentStatus.RESERVED;
+	private GetDoctorSimpleDto doctor;
+	private GetAllPatientDto patient;
+	private List<resNoteAppointment> doctorNotes;
+
 
 }
--- src/main/java/com/carecure/medsysten/dtos/appointment/GetAppointmentSimpleDto.java
+++ src/main/java/com/carecure/medsysten/dtos/appointment/GetAppointmentSimpleDto.java
@@ -8,17 +8,18 @@ import lombok.Data;
 import java.util.Date;
 
 @Data
-public class NewAppointmentDto
+public class GetAppointmentSimpleDto
 {
-
+	private long code;
 	private EnumSpeciality speciality;
 	private Date dateCreated;
 	private Date dateToVisit;
 	private EnumAppointmentType type;
-	private String creationNote;
-	private String userCreatedBy;
+	private EnumAppointmentStatus status;
+	private String notes;
+	private String userLoggerName;
 	private long patientCode;
-	private long doctorCode;
-	private EnumAppointmentStatus status = EnumAppointmentStatus.RESERVED;
+	private String patientName;
+	private String doctorName;
 
 }
