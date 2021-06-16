673141417b66fc9fbc70ebc148b83de89ccb5328
mhaitham <mhaitham@sumerge.com>
2021-06-17 00:18:06 +0200

    Added Misc Enums

---
+++ src/main/java/com/carecure/medsysten/enums/misc/EnumGender.java
@@ -0,0 +1,6 @@
+package com.carecure.medsysten.enums.misc;
+
+public enum EnumGender
+{
+	MALE,FEMALE
+}
---
+++ src/main/java/com/carecure/medsysten/enums/misc/EnumSpeciality.java
@@ -0,0 +1,6 @@
+package com.carecure.medsysten.enums.misc;
+
+public enum EnumSpeciality
+{
+	SURGERY,PLASTICSURGERY,DERMATOLOGY,NUTRITION,INTERNALMEDICINE,OBSTETRICS,DENTISTRY
+}
