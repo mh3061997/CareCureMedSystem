155c748e665de9a153116bdc450ad57aa09a387b
mhaitham <mhaitham@sumerge.com>
2021-06-17 00:05:04 +0200

    Added Appointments Enums

---
+++ src/main/java/com/carecure/medsysten/enums/appointment/EnumAppointmentStatus.java
@@ -0,0 +1,6 @@
+package com.carecure.medsysten.enums.appointment;
+
+public enum EnumAppointmentStatus
+{
+	INVOICED,RESERVED,DOCTORDONE
+}
---
+++ src/main/java/com/carecure/medsysten/enums/appointment/EnumAppointmentType.java
@@ -0,0 +1,6 @@
+package com.carecure.medsysten.enums.appointment;
+
+public enum EnumAppointmentType
+{
+	VISIT,FOLLOWUP
+}
