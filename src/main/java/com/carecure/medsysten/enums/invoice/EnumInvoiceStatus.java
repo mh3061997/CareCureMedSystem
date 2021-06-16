6fa9309bb30e4b767e1c6f1c39ee0861a6abb24c
mhaitham <mhaitham@sumerge.com>
2021-06-17 00:35:37 +0200

    Added Enums

---
+++ src/main/java/com/carecure/medsysten/enums/invoice/EnumInvoiceStatus.java
@@ -0,0 +1,6 @@
+package com.carecure.medsysten.enums.invoice;
+
+public enum EnumInvoiceStatus
+{
+NOTPAID,FINALIZED,DEBT
+}
---
+++ src/main/java/com/carecure/medsysten/enums/misc/EnumPaymentMethod.java
@@ -0,0 +1,6 @@
+package com.carecure.medsysten.enums.misc;
+
+public enum EnumPaymentMethod
+{
+	CASH,VISA
+}
