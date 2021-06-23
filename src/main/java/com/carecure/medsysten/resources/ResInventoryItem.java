f497846813f9557d0909d706051470133250adaf
mhaitham <mhaitham@sumerge.com>
2021-06-24 01:49:21 +0200

    -Added Min Heap Expiry Date Logic

--- src/main/java/com/carecure/medsysten/resources/ResInventoryItem.java
+++ src/main/java/com/carecure/medsysten/resources/ResInventoryItem.java
@@ -1,12 +1,16 @@
 package com.carecure.medsysten.resources;
 
 import com.carecure.medsysten.enums.EnumInventoryItemCategory;
+import com.carecure.medsysten.services.ServInventoryOrder;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.persistence.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Collections;
 import java.util.Date;
 import java.util.Map;
 
@@ -19,6 +23,8 @@ import java.util.Map;
 @ToString
 public class ResInventoryItem
 {
+	private static final Logger logger = LoggerFactory.getLogger(ServInventoryOrder.class.getName());
+
 	private static SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
 	private static SimpleDateFormat dateTimeFormatter = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss.SSSZ");
 	@Id
@@ -58,7 +64,7 @@ public class ResInventoryItem
 		}
 	}
 
-	public boolean deductExpiryDateCount(Date key, int count) throws ParseException
+	public boolean deductExpiryDateCountFromDate(Date key, int count) throws ParseException
 	{
 		//change from datetime to date to match the DB's format to avoid duplicates in the map
 		key = dateFormatter.parse(dateTimeFormatter.format(key));
@@ -73,4 +79,40 @@ public class ResInventoryItem
 		return false;
 
 	}
+
+	public void deductExpiryDateCountOldest(int count) throws ParseException
+	{
+
+		logger.info("Will Deduct {}",count);
+
+		int remainingCount = count;
+
+		while (remainingCount > 0)
+		{
+			logger.info("remaining to Deduct {}",remainingCount);
+
+			Date key = Collections.min(this.expiryDates.keySet());
+
+			int availableCountAtDate = this.expiryDates.get(key);
+
+			if (availableCountAtDate >= remainingCount)
+			{
+				logger.info("{} sufficient to Deduct {}",key,remainingCount);
+				this.expiryDates.put(key, availableCountAtDate - remainingCount);
+				if(this.expiryDates.get(key)==0){
+					logger.info("{} now emptying deleting",key,remainingCount);
+					this.expiryDates.remove(key);
+				}
+			}
+			else
+			{
+				logger.info("{} not sufficient to Deduct {} deleting",key,remainingCount);
+				this.expiryDates.remove(key);
+			}
+
+			remainingCount -= availableCountAtDate;
+		}
+
+	}
+
 }
