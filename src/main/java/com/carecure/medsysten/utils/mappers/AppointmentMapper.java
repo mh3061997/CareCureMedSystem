18cf1ab76585a9d87b29718ffd0fe1c117e23d3a
mhaitham <mhaitham@sumerge.com>
2021-06-29 17:24:04 +0200

    Mapper Fix

--- src/main/java/com/carecure/medsysten/utils/mappers/AppointmentMapper.java
+++ src/main/java/com/carecure/medsysten/utils/mappers/AppointmentMapper.java
@@ -4,16 +4,18 @@ import com.carecure.medsysten.dtos.appointment.GetAppointmentSimpleDto;
 import com.carecure.medsysten.resources.resAppointment;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class AppointmentMapper
 {
 	@Autowired
 	private ModelMapper mapper;
 
-	public GetAppointmentSimpleDto mapAppointmentDaoToSimpleDto(GetAppointmentSimpleDto dao)
+	public GetAppointmentSimpleDto mapAppointmentDaoToSimpleDto(resAppointment dao)
 	{
 		return mapper.map(dao, GetAppointmentSimpleDto.class);
 	}
