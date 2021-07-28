6697353a915a6a933f6fbf14b48f7664ff53e451
mhaitham <mhaitham@sumerge.com>
2021-07-28 23:14:56 +0200

    Replaced Enums with strings

--- src/main/java/com/carecure/medsysten/dtos/invoice/GetInvoiceFullDto.java
+++ src/main/java/com/carecure/medsysten/dtos/invoice/GetInvoiceFullDto.java
@@ -3,8 +3,7 @@ package com.carecure.medsysten.dtos.invoice;
 import com.carecure.medsysten.dtos.appointment.GetAppointmentSimpleDto;
 import com.carecure.medsysten.dtos.membership.GetMembershipSimpleDto;
 import com.carecure.medsysten.dtos.patient.GetPatientSimpleDto;
-import com.carecure.medsysten.enums.invoice.EnumInvoiceStatus;
-import com.carecure.medsysten.enums.misc.EnumPaymentMethod;
+import com.carecure.medsysten.resources.resInvoiceItem;
 import lombok.Data;
 
 import java.util.Date;
@@ -20,12 +19,12 @@ public class GetInvoiceFullDto
 	private long totalAfterDiscount;
 	private long totalPaid;
 	private long totalRemaining;
-	private EnumInvoiceStatus status;
+	private String status;
 	private int discount;
 	private String userFinalizedBy;
 	private GetAppointmentSimpleDto appointment;
-	private EnumPaymentMethod paymentMethod;
-	private List<GetInvoiceSimpleDto> invoiceItems;
+	private String paymentMethod;
+	private List<resInvoiceItem> invoiceItems;
 	private GetMembershipSimpleDto usedMembership;
 	private GetPatientSimpleDto patientMembershipSubscriber;
 }
