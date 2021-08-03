1ee82782aa939157238b94acc0b866e55b1b016d
mhaitham <mhaitham@sumerge.com>
2021-08-03 12:53:49 +0200

    Deleted Note Appointment Resource

--- src/main/java/com/carecure/medsysten/repositories/repoNoteAppointment.java
+++
@@ -1,8 +0,0 @@
-package com.carecure.medsysten.repositories;
-
-import com.carecure.medsysten.resources.resMedImage;
-import com.carecure.medsysten.resources.resNoteAppointment;
-import org.springframework.data.repository.CrudRepository;
-
-public interface repoNoteAppointment extends CrudRepository<resNoteAppointment,Long> {
-}
